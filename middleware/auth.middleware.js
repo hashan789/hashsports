f65a2ae3fc0f3d1fdf2f830d4d53676246656b46
hashan789 <hashanmalind123@gmail.com>
2025-01-05 14:37:38 +0530

    created protect route for admins

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,42 @@
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+
+    try {
+        const accessToken = req.cookies.accessToken;
+
+        if (!accessToken) {
+            return res.status(401).json({ message: "Unauthorized - No access toekn provided" });
+        }
+        try{
+            const decoded = jwt.verify(accessToken, process.env.ACCESS_JWT_SECRET);
+            const user = await User.findById(decoded.userId).select("-password");
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            req.user = user;
+
+            next();
+        } catch (error) {
+           if(error.name === "TokenExpiredError"){
+               return res.status(401).json({message: "Unauthorized - Access token expired"});
+           }
+
+           throw error;
+        }
+
+    } catch (error) {
+        return res.status(401).json({ message: error.message });
+    }
+}
+
+export const adminRoute = async (req, res, next) => {
+
+    if (req.user && req.user.role === "admin") {
+        next();
+    } else {
+        return res.status(403).json({ message: "Forbidden - Admin access required" });
+    }
+}
