a5c895d66988fb7f7032d0b941606ceee0be3d43
hashan789 <hashanmalind123@gmail.com>
2025-01-06 23:36:12 +0530

    added route for cart

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
+import cartRoutes from './routes/cart.route.js';
 
 import { connectDB } from './lib/db.js'
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/product", productRoutes)
+app.use("/api/cart", cartRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log('Server running on port 5000');
