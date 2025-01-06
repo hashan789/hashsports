b4237b1796490831a1a8c4dac05d521c365d480b
hashan789 <hashanmalind123@gmail.com>
2025-01-06 23:37:00 +0530

    added controllers for cart

---
+++ controllers/cart.controller.js
@@ -0,0 +1,88 @@
+export const addToCart = async (req, res) => {
+    try {
+        
+        const { productId } = req.body;
+        const user = req.user;
+
+        const existingProduct = user.cartItems.find(product => product.productId === productId);
+
+        if (existingProduct) {
+            existingProduct.quantity += 1;
+        } else {
+            user.cartItems.push(productId);
+        }
+
+        await user.save();
+
+        res.json(user.cartItems);
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+export const removeAllFromCart = async (req, res) => {
+    try{
+        const { productId } = req.body;
+        const user = req.user;
+        if(productId){
+            user.cartItems = user.cartItems.filter(product => product.productId !== productId);
+        } else {
+            user.cartItems = [];
+        }
+
+        await user.save();
+        res.json(user.cartItems);
+    }
+    catch(error){
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+export const updateQuantity = async (req, res) => {
+    try {
+        
+        const { id : productId } = req.params;
+        const quantity = req.body;
+        const user = req.user;
+        const existingProduct = user.cartItems.find((item) => item.id === productId);
+
+        if(existingProduct){
+            if(quantity === 0){
+                user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+                await user.save();
+                return res.json(user.cartItems);
+            }
+
+            existingProduct.quantity = quantity;
+            await user.save();
+            res.json(user.cartItems);
+        }
+        else{
+            res.status(404).json({ message: "Product not found" });
+        }
+
+    } catch (error) {
+        
+    }
+}
+
+export const getCartProducts = async (req, res) => {
+
+    try {
+        const products = await Product.find({ _id: { 
+            $in : req.user.cartItems.map(item => item.productId) 
+        } });
+
+        //add quantity for each product
+        const cartItems = products.map((product) => {
+            const item = req.user.cartItems.find(cartItem => cartItem.productId === product.id);
+            return {
+                ...product.toJSON(),
+                quantity: item.quantity
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}    
