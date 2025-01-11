f02bbd76a9f25c9aeab75ee5d736edbe5e8678c2
hashan789 <hashanmalind123@gmail.com>
2025-01-11 17:40:35 +0530

    added main route for coupons

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
+import couponRoutes from './routes/coupon.route.js';
 
 import { connectDB } from './lib/db.js'
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/product", productRoutes)
 app.use("/api/cart", cartRoutes)
+app.use("/api/coupon", couponRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log('Server running on port 5000');
