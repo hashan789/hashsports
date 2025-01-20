b718a1e86dcd01efd32099f400de65fbc436d481
hashan789 <hashanmalind123@gmail.com>
2025-01-20 21:37:58 +0530

    added export router

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -7,3 +7,5 @@ const router = express.Router();
 router.post("/create-checkout-session", protectRoute, createCheckoutSession)
 
 router.post("/checkout-session", protectRoute, checkoutSuccess)
+
+export default router;
