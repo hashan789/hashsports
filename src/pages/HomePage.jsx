f45d4f7a1af5ceb38ad8e714821255d0c4db2ccf
hashan789 <hashanmalind123@gmail.com>
2025-01-23 21:11:51 +0530

    added cover page and products list

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
-
+import Cover from "../components/Cover";
+import ProductsShow from "../components/ProductsShow";
+import { motion } from "framer-motion";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <motion.div 
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+    >
+      <Cover/>
+      <ProductsShow/>
+    </motion.div>
   )
 }
