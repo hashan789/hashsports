e31bc056afcc3d69d6136888072f90707505a4dc
hashan789 <hashanmalind123@gmail.com>
2025-01-29 23:59:23 +0530

    added routes for cart and category

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
@@ -6,14 +6,25 @@ import NavBar from "./components/NavBar"
 import { useUserState } from "./stores/useUserState"
 import { useEffect } from "react"
 import AdminPage from "./pages/AdminPage"
+import CategoryPage from "./pages/CategoryPage"
+import CartPage from "./pages/CartPage"
+import { useCartState } from "./stores/useCartState"
 
 function App() {
 
   const { user, checkAuth } = useUserState();
 
+  const { getCartItems } = useCartState();
+
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+
+  useEffect(() => {
+    if(user){
+      getCartItems();
+    }
+  }, [getCartItems,user]);
 
   return (
     <>
@@ -22,8 +33,10 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={ user ? <HomePage/> : <LoginPage />} />
+          <Route path="/login" element={ !user ? <LoginPage /> : <Navigate to={'/'} /> } />
           <Route path="/dashboard" element={ user?.role === 'admin' ? <AdminPage /> : <LoginPage />} />
+          <Route path="/cart" element={ user ? <CartPage /> : <Navigate to={'/login'} />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
         </Routes>
       </div>
     </>
