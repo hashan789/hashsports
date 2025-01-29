526747e06122a9f3c31a1d98412262178afc5e58
hashan789 <hashanmalind123@gmail.com>
2025-01-30 00:04:11 +0530

    created dashboard page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,37 +1,62 @@
 import { PenBox, LucideShoppingCart , ChartNoAxesCombinedIcon } from "lucide-react";
+import { useState , useEffect } from "react";
+import CreateProductForm from "../components/CreateProductForm";
+import AnalyticsTab from "../components/AnalyticsTab";
+import ProductsList from "../components/ProductsList";
+import { useProductState } from "../stores/useProductState";
+// import tabs from '../documents/tab.json'
 
 export default function AdminPage() {
+
+  const tabs = [
+    {
+        id: "create",
+        label: "Create Product",
+        icon: PenBox
+    },
+    {
+        id: "products",
+        label: "Products",
+        icon: LucideShoppingCart
+    },
+    {
+        id: "analytics",
+        label: "Analytics",
+        icon: ChartNoAxesCombinedIcon
+    }
+]
+
+  const { getAllProducts } = useProductState();
+
+  useEffect(() => {
+    getAllProducts();
+  }, [getAllProducts]);
+
+  const [activeTab, setActiveTab] = useState("create")
+
   return (
-    <div className="mx-auto flex justify-center relative">
-      <div className="w-2/12 h-screen bg-blue-500">
-        <div className="w-full bg-white h-auto relative top-20 font-semibold">
-          <div className="w-full bg-purple-600 h-20">
-            <div className="flex justify-center items-center relative top-1/3 cursor-pointer">
-              <span className="text-white">Create Product</span>
-              <span className="ml-3"><PenBox className="text-white" width={20} /></span>
-            </div>
-          </div>
-          <div className="w-full bg-purple-700 h-20">
-            <div className="flex justify-center items-center relative top-1/3 cursor-pointer">
-              <span className="text-white">Products</span>
-              <span className="ml-3"><LucideShoppingCart className="text-white" width={20} /></span>
-            </div>
-          </div>
-          <div className="w-full bg-purple-800 h-20">
-          <div className="flex justify-center items-center relative top-1/3 cursor-pointer">
-              <span className="text-white">Analytics</span>
-              <span className="ml-3"><ChartNoAxesCombinedIcon className="text-white" width={20} /></span>
-            </div>
-          </div>
+    <div className="mx-auto flex justify-center relative min-h-screen h-auto">
+      <div className="w-2/12 min-h-screen h-auto bg-blue-500">
+        <div className="w-full h-auto relative top-20 font-semibold">
+          {
+            tabs.map((tab) => (
+              <div key={tab.id} className="w-full h-20">
+                <div className="flex justify-center items-center relative top-1/3 cursor-pointer" onClick={() => setActiveTab(tab.id)}>
+                  <span className={ activeTab === tab.id ? "text-white" : "text-blue-950"}>{tab.label}</span>
+                  <span className="ml-3"><tab.icon className={ activeTab === tab.id ? "text-white" : "text-blue-950"} width={20} /></span>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
-      <div className="w-10/12 h-screen bg-slate-600 grid p-5">
-        <div className="w-full h-24created bg-blue-700 relative top-1/4 ">
-          {/* <div className="w-10/12 h-full bg-blue-200"></div> */}
-        </div>
-        <div className="w-full h-52 bg-blue-300">
-          {/* <div className="w-10/12 h-full bg-blue-200"></div> */}
-        </div>
+
+      <div className="w-10/12 min-h-screen h-full bg-slate-600 grid pt-28 px-5">
+        
+        { activeTab === 'create' && <CreateProductForm /> }
+        { activeTab === 'products' && <ProductsList /> }
+        { activeTab === 'analytics' && <AnalyticsTab /> }
+
       </div>
     </div>
   )
