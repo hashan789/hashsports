3d17b8b4c94dcb2357a48d887d5ea77af73d4769
hashan789 <hashanmalind123@gmail.com>
2025-01-30 00:06:31 +0530

    created methods for products operations

---
+++ src/stores/useProductState.js
@@ -0,0 +1,94 @@
+import { create } from 'zustand'
+import toast from 'react-hot-toast'
+import axios from '../lib/axios'
+
+export const useProductState = create((set) => ({
+    products: [],
+    loading: false,
+
+    setProducts: (products) => set({products}),
+
+    createProduct: async (productData) => {
+        set({ loading: true })
+
+        try {
+            const res = await axios.post('/product/', productData);
+            set((prevState) => ({
+                products : [...prevState.products, res.data],
+                loading: false
+            }));
+            return toast.success("Added product successfully!");
+        } catch (error) {
+            set({ loading: false })
+            return toast.error(error.response.data.error);
+        }
+
+    },
+
+    getAllProducts: async () => {
+
+        set({ loading : true });
+
+        try {
+            const res = await axios.get('/product/');
+            set({ products: res.data.products, loading: false });
+        }
+        catch(error){
+            set({ loading: false });
+            return toast.error(error.response.data.error);
+        }
+    },
+
+    fetchProductsByCategory: async (category) => {
+
+        set({ loading : true });
+
+        try{
+            const res = await axios.get(`/product/category/${category}`);
+            set({ products: res.data.products, loading: false });
+        }
+        catch(error){
+            set({ loading: false });
+            toast.error(error.response.data.error);
+        }
+    },
+
+    deleteProduct: async (productId) => {
+        set({ loading: true });
+
+        try{
+            await axios.delete(`/product/${productId}`);
+            set((prevProducts) => ({
+                products: prevProducts.products.filter((product) => product._id !== productId),
+                loading: false
+            }));
+            return toast.success("Product deleted successfully!");
+        }
+        catch(error){
+            set({ loading: false });
+            return toast.error(error.response.data.error);
+        }
+    },
+
+    toggleFeaturedProducts: async (productId) => {
+        set({ loading: true });
+
+        try{
+            const response = await axios.patch(`/product/${productId}`);
+            set((prevProduct) => ({
+                products: prevProduct.products.map((product) => (
+                    product._id === productId ?
+                        {...product, isFeatured: response.data.isFeatured}
+                    : product
+                )),
+                loading: false
+            }));
+        }
+        catch(error){
+            set({ loading: false });
+            return toast.error(error.response.data.error);
+        }
+    }
+
+
+}))
