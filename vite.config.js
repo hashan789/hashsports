12b85c726de5b8a8fe1d7b6f43b3f76d0aab28a4
hashan789 <hashanmalind123@gmail.com>
2025-02-02 17:07:14 +0530

    added dotenv config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-      },
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.STRIPE_PUBLISH_KEY': JSON.stringify(env.STRIPE_PUBLISH_KEY)
     },
+    plugins: [react()],
+    server:{
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+        },
+      },
+    }
   }
 })
