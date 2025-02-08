0abdfb6bd3ec6a82740b039b391f1b2e675a8ab1
hashan789 <hashanmalind123@gmail.com>
2025-02-08 14:43:47 +0530

    created template footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+
+
+export default function Footer() {
+  return (
+    <div>
+        <div className="bg-gray-800 text-white py-10">
+            <div className="container mx-auto flex justify-between items-center">
+            <div>
+                <h1 className="text-3xl font-bold">Hashsports</h1>
+                <p className="text-sm">© 2021 Hashsports. All rights reserved.</p>
+            </div>
+            <div>
+                <h1 className="text-xl font-bold">Links</h1>
+                <ul className="block space-y-4 mt-3">
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Brands</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div>
+                <h1 className="text-xl font-bold">Contact</h1>
+                <p className="text-sm">Hashsports, 123 Main Street, City, State, 12345</p>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
