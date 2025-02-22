de175bf0af415d027d64bdc03cf908fcaa3d9f72
hashan789 <hashanmalind123@gmail.com>
2025-02-22 21:18:30 +0530

    modified UI

--- src/components/FeaturedProductsShow.jsx
+++ src/components/FeaturedProductsShow.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import ProductCard from "./ProductCard";
-import { ChevronLeft, ChevronRight } from "lucide-react";
 
 
 export default function FeaturedProductsShow({ products }) {
 
-    const [ currentIndex, setCurrentIndex ] = useState(0);
     const [ itemsPerPage, setItemsPerPage ] = useState(5);
 
     useEffect(() => {
@@ -23,55 +23,24 @@ export default function FeaturedProductsShow({ products }) {
 
     },[]);
 
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) => prevIndex + currentIndex);
-        console.log(currentIndex)
-    }
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => prevIndex - currentIndex);
-        console.log(currentIndex)
-    }
-
-    const isStartDisabled = currentIndex === 0;
-    const isEndDisabled = currentIndex >= ( products.length - itemsPerPage );
-
 
   return (
     <div className="py-12">
     <div className="container mx-auto px-4">
         <div className="">
-            <div className="w-full overflow-hidden flex justify-center">
-                <div className="flex gap-5 w-full transition-transform duration-300" style={{ 
-                                transform: `translateX(-${(currentIndex * (100 / itemsPerPage))}%)`
-                }}>
-                    {
-                        products?.map((product,index) => (
-                            <div key={index} className="w-1/4 flex-shrink">
-                                <ProductCard product={product} />
-                            </div>
-                        ))
-                    }
-                 </div>
+            <div className="w-full">
+            <div
+                className="flex justify-center"
+            >
+                {products.map((product,index) => (
+                <div key={index} size={100} className="p-4 text-center">
+                    <ProductCard product={product} />
+                </div>
+                ))}
+            </div>
             </div>
         </div>
-        <button
-            disabled={isStartDisabled}
-            className={
-                `absolute -left-4 bottom-1/2 -translate-y-1/4 p-2 rounded-full transition-colors duration-300 ${ isStartDisabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500' }`
-            }
-            onClick={prevSlide}
-        >
-            <ChevronLeft size={20} className="object-cover"/>
-        </button>
-        <button
-            disabled={isEndDisabled}
-            className={
-                `absolute -right-4 bottom-1/2 -translate-y-1/4 p-2 rounded-full transition-colors duration-300 ${ isEndDisabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500' }`
-            }
-            onClick={nextSlide}
-        >
-            <ChevronRight size={20} className="object-cover"/>
-        </button>
+    
     </div>
     </div>
   )
