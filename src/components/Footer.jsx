306446520d60d8104329d2ba7c0f64f41817028d
hashan789 <hashanmalind123@gmail.com>
2025-02-25 21:02:40 +0530

    modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,8 @@ export default function Footer() {
         <div className="bg-gray-800 text-white py-10">
             <div className="container mx-auto flex justify-between items-center">
             <div>
-                <h1 className="text-3xl font-bold">Hashsports</h1>
-                <p className="text-sm">© 2021 Hashsports. All rights reserved.</p>
-            </div>
-            <div>
                 <h1 className="text-xl font-bold">Links</h1>
-                <ul className="block space-y-4 mt-3">
+                <ul className="flex justify-center gap-5 mt-3 text-sm">
                     <li>Home</li>
                     <li>Products</li>
                     <li>Brands</li>
@@ -23,6 +19,11 @@ export default function Footer() {
                 <p className="text-sm">Hashsports, 123 Main Street, City, State, 12345</p>
             </div>
         </div>
+            <div className="flex justify-center items-center">
+                <div className="">
+                    <p className="text-sm">© 2021 Hashsports. All rights reserved.</p>
+                </div>
+            </div>
     </div>
     </div>
   )
