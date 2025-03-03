c4cf340bb5c2815b99fcea5f5fd2c06d64ac0b59
hashan789 <hashanmalind123@gmail.com>
2025-03-03 19:08:21 +0530

    enabled loading .env

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const protectRoute = async (req, res, next) => {
 
