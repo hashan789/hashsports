0d02faac18c23d78037ea7d4fd1c21cd36be96ce
hashan789 <hashanmalind123@gmail.com>
2025-03-05 22:40:38 +0530

    added server API (product)

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "https://hashsports-server-hmcxdehwapcwa2a4.canadacentral-01.azurewebsites.net/api/",
+    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api/" : "https://hashsports-server-hmcxdehwapcwa2a4.canadacentral-01.azurewebsites.net/api",
     withCredentials: true
 })
 
