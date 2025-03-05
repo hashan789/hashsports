45f78d56a2419e18ef4822b6c1e484cb478b74ee
hashan789 <hashanmalind123@gmail.com>
2025-03-05 21:42:46 +0530

    removed API mode

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "production" ? "https://hashsports-server-hmcxdehwapcwa2a4.canadacentral-01.azurewebsites.net/api/" : "/api",
+    baseURL: "https://hashsports-server-hmcxdehwapcwa2a4.canadacentral-01.azurewebsites.net/api/",
     withCredentials: true
 })
 
