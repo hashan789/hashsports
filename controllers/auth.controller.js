bd5cab1936eb9dcd249ff1d47897707a6a182a65
hashan789 <hashanmalind123@gmail.com>
2025-03-05 22:56:50 +0530

    added httpOnly

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -19,11 +19,15 @@ const storeRefreshToken = async (userId, refreshToken) => {
 
 const setCookies = (res, accessToken, refreshToken) => {
     res.cookie("accessToken", accessToken, {
-        httpOnly: false,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: "strict",
         maxAge: 15 * 60 * 1000
     });
     res.cookie("refreshToken", refreshToken, {
-        httpOnly: false,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
 }
