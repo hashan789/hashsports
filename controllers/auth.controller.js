e025804bfb09724c09f4dde26dfe829cb6892d09
Hashan Malinda <85447795+hashan789@users.noreply.github.com>
2025-03-06 12:27:18 +0530

    changed secure to true

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -20,13 +20,13 @@ const storeRefreshToken = async (userId, refreshToken) => {
 const setCookies = (res, accessToken, refreshToken) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: "None",
         maxAge: 15 * 60 * 1000
     });
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
