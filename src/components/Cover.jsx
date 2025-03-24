3d3b221d39f596a548e8e122bc6d0b9f31d4e27a
hashan789 <hashanmalind123@gmail.com>
2025-03-24 20:13:12 +0530

    added mobile responsive design

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -5,11 +5,11 @@ export default function Cover() {
     <div className={`bg-[url("/images/cover_page.png")] w-full bg-center h-screen`}> 
         <div className="w-full flex justify-center items-center top-80 absolute">
             <div className="w-7/12 text-center">
-                <h1 className="text-7xl text-white font-bold">Grab the chance</h1>
-                 <h1 className="text-2xl text-white p-4">
+                <h1 className="lg:text-7xl max-sm:text-2xl text-white font-bold">Grab the chance</h1>
+                 <h1 className="lg:text-2xl max-sm:text-sm text-white p-4">
                  Don{"'"}t miss out on exclusive deals and limited-time offers! 
                  </h1>
-                <button className="rounded-xl mt-5 pl-10 pr-10 pt-3 pb-3 border text-center border-blue-300 text-blue-300">See more</button>
+                <button className="rounded-xl lg:mt-5 lg:pl-10 lg:pr-10 max-sm:pl-5 max-sm:pr-5 pt-3 pb-3 border text-center border-blue-300 text-blue-300">See more</button>
             </div>
         </div>
     </div>
--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -13,9 +13,10 @@ export default function FeaturedProducts() {
   return (
     <div>
         <div className='flex justify-center items-center p-10 mb-10'>
-        <div className="top-12 relative">
-            <h1 className="text-4xl text-center font-semibold">Featured Products</h1>
-            <div className="mt-12 flex justify-center items-center gap-6">
+        <div className="top-12 relative max-sm:w-full">
+            <h1 className="lg:text-4xl max-sm:text-2xl text-center font-semibold">Featured Products</h1>
+            <h1 className="lg:text-lg max-sm:text-sm text-center mt-8">Check out our handpicked selection of best-selling and top-rated products. </h1>
+            <div className="mt-5 flex justify-center items-center gap-6">
                 {
                    !isLoading && products.length > 0 && <FeaturedProductsShow products={products} />
                 }
