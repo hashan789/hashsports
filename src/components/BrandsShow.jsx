51c2ecb51705fc6eacb6f26180d93da041082b6b
hashan789 <hashanmalind123@gmail.com>
2025-03-29 11:22:25 +0530

    added brands images

--- src/components/BrandsShow.jsx
+++ src/components/BrandsShow.jsx
@@ -1,15 +1,16 @@
+
 export default function BrandsShow() {
 
-  const brands = ['/images/brands/dsi.png', '/images/brands/Nike-Logo.png', '/images/brands/moose.jpeg'];
+  const brands = ['/images/brands/dsi.png', '/images/brands/Nike-Logo.png', '/images/brands/adidas.png', '/images/brands/fila.png', '/images/brands/levis.png', '/images/brands/puma.jpg'];
 
   return (
     <div className="flex justify-center items-center mb-10">
       <div className="">
-        <h1 className="text-center text-4xl font-bold my-10">Brands</h1>
-        <div className="flex justify-around items-center gap-10">
+        <h1 className="text-center lg:text-4xl max-sm:text-2xl font-bold my-10">Brands</h1>
+        <div className="lg:flex lg:justify-center lg:items-center max-sm:grid max-sm:grid-cols-3 lg:gap-10 max-sm:gap-5">
           {
             brands.map((brand, index) => (
-              <img key={index} src={brand} alt="brand" width={150} className="brand-image mix-blend-hard-light" />
+              <img key={index} src={brand} alt="brand" className="lg:w-36 max-sm:w-16 brand-image mix-blend-hard-light" />
             ))
           }
         </div>
