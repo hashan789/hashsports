8086176024bf5adaa01efe13599029f5a671957b
hashan789 <hashanmalind123@gmail.com>
2025-04-09 20:13:50 +0530

    modified cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -7,18 +7,20 @@ export default function CartItem({ item }) {
 
   return (
     <div className="max-sm:flex max-sm:justify-center max-sm:items-center">
-        <div className="flex items-center gap-4 border p-5">
+        <div className="flex items-center justify-around p-5">
             <div className="w-20 h-20">
             <img src={item.image} alt={item.name} className="w-full h-full object-cover rounded-lg" />
             </div>
-            <div className="flex-1">
-            <h1 className="text-lg font-semibold">{item.name}</h1>
-            <p className="text-sm text-gray-500">Price: ${item.price}</p>
-            <div className="flex items-center gap-4">
-                <button onClick={() => updateQuantity(item._id, item.quantity - 1)} className="text-sm font-semibold text-gray-500"><Minus /></button>
-                <p className="text-sm font-semibold">{item.quantity}</p>
-                <button onClick={() => updateQuantity(item._id, item.quantity + 1)} className="text-sm font-semibold text-gray-500"><Plus /></button>
-            </div>
+            <div className="flex justify-center items-center">
+              <div className="flex-1">
+              <h1 className="text-lg font-semibold">{item.name}</h1>
+              <p className="text-sm text-gray-500">Price: ${item.price}</p>
+              <div className="flex items-center gap-4">
+                  <button onClick={() => updateQuantity(item._id, item.quantity - 1)} className="text-sm font-semibold text-gray-500"><Minus /></button>
+                  <p className="text-sm font-semibold">{item.quantity}</p>
+                  <button onClick={() => updateQuantity(item._id, item.quantity + 1)} className="text-sm font-semibold text-gray-500"><Plus /></button>
+              </div>
+              </div>
             </div>
             <button onClick={() => removeFromCart(item._id)} className="text-red-500 font-semibold"><Trash /></button>
         </div>
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -17,15 +17,15 @@ export default function CartPage() {
       transition={{ duration: 0.8 }}
       className="container mx-auto lg:flex max-sm:block justify-center relative top-20"
     >
-      <div className="lg:w-9/12">
+      <div className="lg:w-8/12">
           {
             cart.length === 0 ? (
               <EmptyCart />
             ) : (
               <div className="container mx-auto">
                 <h1 className="text-2xl max-sm:text-center font-semibold">Your Cart</h1>
-                <div className="max-sm:flex max-sm:justify-center max-sm:items-center">
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4 my-5 w-1/2">
+                <div className="max-sm:flex max-sm:justify-center max-sm:items-center p-4">
+                  <div className="w-full p-4 border shadow-md rounded-lg">
                     {
                       cart.map((item,index) => (
                         <CartItem key={index} item={item} />
@@ -44,7 +44,7 @@ export default function CartPage() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
-            className="lg:w-3/12"
+            className="lg:w-4/12"
           >
             <div className="space-y-4">
               <OrderSummory />
