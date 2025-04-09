10c38dac14cc5b52e619026acd08f1b8ebddc9f4
hashan789 <hashanmalind123@gmail.com>
2025-04-09 20:23:50 +0530

    created 'remove all items' controller

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addToCart, getCartProducts, removeAllFromCart, updateQuantity } from "../controllers/cart.controller.js";
+import { addToCart, getCartProducts, removeAllFromCart, removeAllItems, updateQuantity } from "../controllers/cart.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -9,6 +9,8 @@ router.get("/", protectRoute, getCartProducts);
 router.post("/", protectRoute, addToCart);
 
 router.delete("/", protectRoute, removeAllFromCart);
+
+router.delete("/user_items", protectRoute, removeAllItems);
 
 router.put("/:id", protectRoute, updateQuantity);
 
