62222be2ac329ae2cec7cf47932ad70b53173915
hashan789 <hashanmalind123@gmail.com>
2025-04-11 18:41:14 +0530

    added 'Time' column in Orders Table

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -2,7 +2,7 @@ import { useCartState } from '../stores/useCartState';
 
 export default function Orders() {
 
-  const columns = ["Date", "Price", "quantity", "Amount"];
+  const columns = ["Date", "Time", "Price", "quantity", "Amount ($)"];
 
   const { orders } = useCartState();
 
@@ -10,15 +10,15 @@ export default function Orders() {
 
   return (
     <div>
-      <table className="divide-y divide-white min-w-full">
-        <thead className="bg-gray-700">
+      <table className="divide-y divide-black text-black min-w-full">
+        <thead className="">
           <tr>
             {
               columns.map((column, index) => (
                 <th
                   key={index}
                   scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                  className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
                 >
                   {column}
                 </th>
@@ -33,22 +33,26 @@ export default function Orders() {
                 <tbody key={key}>
                   {
                       order.products?.map((product, index) => ( 
-                        <tr key={index} className="bg-gray-800">
+                        <tr key={index} className="">
+                            {
+                              order.createdAt.split("T").map((item, index) => (
+                              <td key={index} className="px-6 py-4 whitespace-nowrap">
+                                  <div className="flex items-center">
+                                    <div className="ml-4">
+                                      <div className="text-sm font-medium">{item.split(".")[0]}</div>
+                                    </div>
+                                  </div>
+                              </td>
+                              ))
+                            }
                           <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="flex items-center">
-                              <div className="ml-4">
-                                <div className="text-sm font-medium text-white">{order.createdAt}</div>
-                              </div>
-                            </div>
+                            <div className="text-sm">{product.price}</div>
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="text-sm text-white">{product.price}</div>
+                            <div className="text-sm">{product.quantity}</div>
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="text-sm text-white">{product.quantity}</div>
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="text-sm text-white">{ product.price * product.quantity }</div>
+                            <div className="text-sm">{ product.price * product.quantity }</div>
                           </td>
                         </tr>
                       ))
