240557f85f589dd79bb43506aeaba8040a6e1259
hashan789 <hashanmalind123@gmail.com>
2025-04-14 22:50:59 +0530

    added lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
 import { BrowserRouter } from 'react-router-dom'
+import Loader from './components/Loader.jsx'
+
+const App = lazy(() => import('./App.jsx'))
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Suspense fallback={
+      <div className='bg-blue-700 bg-opacity-30 flex justify-center items-center w-full h-screen'>
+        <Loader />
+      </div>
+      }> 
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Suspense>
   </StrictMode>,
 )
